8d0135b48b250c5aac9e4a710ea94a7d4aaff2e7
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-09-01 19:15:00 -0500

    Fix nil garage
    This should fix the error for when you make a new house and it has no garage assigned in the database

--- qb-houses.sql
+++ qb-houses.sql
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS `houselocations` (
   `owned` tinyint(2) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `tier` tinyint(2) DEFAULT NULL,
-  `garage` text DEFAULT NULL,
+  `garage` text NOT NULL DEFAULT '{"y":0,"x":0,"h":0,"z":0}',
   PRIMARY KEY (`id`),
   KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=163 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `player_houses` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
@@ -25,4 +25,4 @@ CREATE TABLE IF NOT EXISTS `player_houses` (
   KEY `house` (`house`),
   KEY `citizenid` (`citizenid`),
   KEY `identifier` (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
