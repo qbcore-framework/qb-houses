9da3d43f0b10b756434997d4c666144ebc6821f2
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-11-14 20:48:17 +0100

    refactor(decorate/entity/outline): Replace outline function with native

--- client/decorate.lua
+++ client/decorate.lua
@@ -212,99 +212,6 @@ local function CheckMovementInput()
 	SetCamCoord(MainCamera, curPos.x, curPos.y, curPos.z)
 end
 
-local function DrawEntityBoundingBox(entity, color)
-	local model = GetEntityModel(entity)
-    local min, max = GetModelDimensions(model)
-    local rightVector, forwardVector, upVector, position = GetEntityMatrix(entity)
-
-    -- Calculate size
-    local dim =
-	{
-		x = 0.5*(max.x - min.x),
-		y = 0.5*(max.y - min.y),
-		z = 0.5*(max.z - min.z)
-	}
-
-    local FUR =
-    {
-		x = position.x + dim.y*rightVector.x + dim.x*forwardVector.x + dim.z*upVector.x,
-		y = position.y + dim.y*rightVector.y + dim.x*forwardVector.y + dim.z*upVector.y,
-		z = 0
-    }
-
-    local FUR_bool, FUR_z = GetGroundZFor_3dCoord(FUR.x, FUR.y, 1000.0, 0)
-    FUR.z = FUR_z
-    FUR.z = position.z + 2 * dim.z
-
-    local BLL =
-    {
-        x = position.x - dim.y*rightVector.x - dim.x*forwardVector.x - dim.z*upVector.x,
-        y = position.y - dim.y*rightVector.y - dim.x*forwardVector.y - dim.z*upVector.y,
-        z = 0
-    }
-    local BLL_bool, BLL_z = GetGroundZFor_3dCoord(FUR.x, FUR.y, 1000.0, 0)
-    BLL.z = position.z
-
-    -- DEBUG
-    local edge1 = BLL
-    local edge5 = FUR
-
-    local edge2 =
-    {
-        x = edge1.x + 2 * dim.y*rightVector.x,
-        y = edge1.y + 2 * dim.y*rightVector.y,
-        z = edge1.z + 2 * dim.y*rightVector.z
-    }
-
-    local edge3 =
-    {
-        x = edge2.x + 2 * dim.z*upVector.x,
-        y = edge2.y + 2 * dim.z*upVector.y,
-        z = edge2.z + 2 * dim.z*upVector.z
-    }
-
-    local edge4 =
-    {
-        x = edge1.x + 2 * dim.z*upVector.x,
-        y = edge1.y + 2 * dim.z*upVector.y,
-        z = edge1.z + 2 * dim.z*upVector.z
-    }
-
-    local edge6 =
-    {
-        x = edge5.x - 2 * dim.y*rightVector.x,
-        y = edge5.y - 2 * dim.y*rightVector.y,
-        z = edge5.z - 2 * dim.y*rightVector.z
-    }
-
-    local edge7 =
-    {
-        x = edge6.x - 2 * dim.z*upVector.x,
-        y = edge6.y - 2 * dim.z*upVector.y,
-        z = edge6.z - 2 * dim.z*upVector.z
-    }
-
-    local edge8 =
-    {
-        x = edge5.x - 2 * dim.z*upVector.x,
-        y = edge5.y - 2 * dim.z*upVector.y,
-        z = edge5.z - 2 * dim.z*upVector.z
-    }
-
-    DrawLine(edge1.x, edge1.y, edge1.z, edge2.x, edge2.y, edge2.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge1.x, edge1.y, edge1.z, edge4.x, edge4.y, edge4.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge2.x, edge2.y, edge2.z, edge3.x, edge3.y, edge3.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge3.x, edge3.y, edge3.z, edge4.x, edge4.y, edge4.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge5.x, edge5.y, edge5.z, edge6.x, edge6.y, edge6.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge5.x, edge5.y, edge5.z, edge8.x, edge8.y, edge8.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge6.x, edge6.y, edge6.z, edge7.x, edge7.y, edge7.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge7.x, edge7.y, edge7.z, edge8.x, edge8.y, edge8.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge1.x, edge1.y, edge1.z, edge7.x, edge7.y, edge7.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge2.x, edge2.y, edge2.z, edge8.x, edge8.y, edge8.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge3.x, edge3.y, edge3.z, edge5.x, edge5.y, edge5.z, color.r, color.g, color.b, color.a)
-    DrawLine(edge4.x, edge4.y, edge4.z, edge6.x, edge6.y, edge6.z, color.r, color.g, color.b, color.a)
-end
-
 -- Events
 
 RegisterNetEvent('qb-houses:client:decorate', function()
@@ -532,8 +439,8 @@ Citizen.CreateThread(function()
             CheckMovementInput()
 
 			if SelectedObj and peanut then
-				local color = {r = 116, g = 189, b = 252, a = 100}
-				DrawEntityBoundingBox(SelectedObj, color)
+		SetEntityDrawOutline(SelectedObj)
+		SetEntityDrawOutlineColor(116, 189, 252, 100)
                 DrawMarker(21, SelObjPos.x, SelObjPos.y, SelObjPos.z + 1.28, 0.0, 0.0, 0.0, 180.0, 0.0, 0.0, 0.6, 0.6, 0.6, 28, 149, 255, 100, true, true, 2, false, false, false, false)
                 if rotateActive then
                     CheckObjRotationInput()
