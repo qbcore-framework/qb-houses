0aa457212b012fe1f3f660081ff4190411669c31
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-01-21 10:05:01 +1030

    Update sk.lua

--- locales/sk.lua
+++ locales/sk.lua
@@ -57,4 +57,7 @@ local Translations = {
         ["lock_door_police"] = "Zatvoriť dvere"
     }
 }
-Lang = Locale:new({phrases = Translations})
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
